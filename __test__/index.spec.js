98cf8c6999c19386748a2d3c19b882ae8e7711d7
Anderson Silva de Oliveira (Phi) <anderson.oliveira@meta.com.br>
2019-10-13 20:58:15 -0300

    Adjust unit test

---
+++ __test__/index.spec.js
@@ -0,0 +1,82 @@
+const chai = require("chai"),
+  should = chai.should(),
+  expect = chai.expect;
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+require("dotenv/config");
+
+/**
+ * @author Anderson Oliveira
+ * @copyright 08/2019
+ * @description Check route root with success
+ */
+describe("GET ROOT", () => {
+
+  it("it should GET ROOT the success status", done => {
+
+    chai
+      .request("http://localhost:5000")
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(JSON.parse(res.text))
+          .to.have.property("status")
+          .equal("Conectado ao Server");
+
+        done();
+      });
+
+  });
+
+});
+
+/**
+ * @author Anderson Oliveira
+ * @copyright 08/2019
+ * @description Check route root with success
+ */
+describe("GET SPOTS", () => {
+
+  let mock;
+
+  before(() => {
+      mock = new MockAdapter(axios, { delayResponse: 2000 });
+  });
+
+  afterEach(() => {
+      mock.reset();
+  });
+
+  after(() => {
+      mock.restore();
+  });
+
+  it("it should GET SPOTS the SUCCESS status", done => {
+
+    let params = {
+      key: process.env.API_KEY_MAPS,
+      query: "tourist spots teste"
+    };
+
+    mock.onGet(process.env.URL_GOOGLE_MAPS, { params }).reply(200, { results: [{ formatted_address: "teste", name: "OK", photos: "teste", opening_hours: { open_now: true} }] });
+
+    chai
+      .request("http://localhost:5000")
+      .post("/api")
+      .set("Content-Type", "application/graphql")
+      .send(`{ query: getSpots(place: "teste") { name } }`)
+      .end((err, res) => {
+
+        console.log(" ==>>", res.body.data.query)
+        res.should.have.status(200);
+        res.body.data.query[0].should.have.property("name");
+
+        done();
+      });
+
+  }).timeout(3000);
+});
--- src/resolvers/resolverSpots.js
+++
@@ -1,9 +0,0 @@
-const spots = {
-  Query: {
-    getSpots() {
-      return { id: 1, address: "Rua Um", picture: "base64" };
-    }
-  }
-};
-
-module.exports = spots;
